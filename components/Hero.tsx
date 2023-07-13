2b9e8a8564dcc7b102d3f1d4b1355c2ccca0d868
sandeep <sandyadav434@gmail.com>
2023-07-13 09:07:00 +0530

    Cars Catalouge

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find ,book, or rent - car quickly and easily!
+          Find ,book, or rent a car - quick and <br /> super easy!
         </h1>
         <p className="hero__subtitle">
           strealine your car rental experience with our effortless booking
